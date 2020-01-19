50755bcfd3b2cdf3230478d7a7971fafeed1b902
thiagola92 <thiagola92@gmail.com>
2020-01-19 11:07:00 -0300

    Basic to do with postgres

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Database
+Os bancos de dados utilizados foram montados utilizando Docker.  
 
 ## Postgres
+`sudo docker pull postgres:11`  
 `sudo docker network create postgres_network`  
 `sudo docker run --name="postgres" --network="postgres_network" --env="POSTGRES_PASSWORD=postgres" --publish 5432:5432 --detach postgres:11`  
 
 ## Mongo
+`sudo docker pull mongo`  
 `sudo docker network create mongo_network`  
 `sudo docker run --name="mongo" --network="mongo_network" --detach mongo`
 
 ## Elasticsearch
+`sudo docker pull elasticsearch:7.5.1`  
 `sudo docker network create elasticsearch_network`  
 `sudo docker run --name="elasticsearch" --network="elasticsearch_network" -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --detach elasticsearch:7.5.1`  
--- run_postgres.py
+++ run_postgres.py
@@ -1,3 +1,31 @@
 import psycopg2
 
 postgres = psycopg2.connect(host='127.0.0.1', dbname='postgres', user='postgres', password='postgres')
+cursor = postgres.cursor()
+
+sql = """CREATE TABLE produtos(
+    sku integer,
+    name varchar(255),
+    description text,
+    category varchar(255)
+)"""
+cursor.execute(sql)
+
+sql = """INSERT INTO produtos
+    VALUES(134218478, 'Rb-01 - Robô Aspirador De Pó Fast Clean Bivolt - Mondial', 'Use a tecnologia a seu favor para aproveitar a vida longe da faxina. Com o Robô Aspirador de Pó Fast Clean RB-01 da Mondial, sua casa fica limpa sem que você precise manusear o aparelho. Esse modelo consegue varrer, aspirar e limpar diversos tipos de superfícies ao circular sozinho pelo ambiente. Tudo isso sem danificar paredes ou móveis. Graças aos seus sensores inteligentes, ele consegue desviar de obstáculos e até mesmo evitar possíveis quedas. Conheça mais essa facilidade para o seu lar e deixe tuuuudo limpinho :)', 'eletroportáteis'
+)"""
+cursor.execute(sql)
+
+sql = """SELECT * FROM produtos"""
+cursor.execute(sql)
+
+products = cursor.fetchall()
+print(products)
+
+sql = """DROP TABLE produtos"""
+cursor.execute(sql)
+
+# important
+cursor.close()
+postgres.commit()
+postgres.close()
